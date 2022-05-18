708e817d8c0dc2943007d4ea0bfce205571ebac0
Debdutta Panda <bedmad82@gmail.com>
2022-05-19 01:19:06 +0530

    190520220118

--- app/src/main/java/com/debduttapanda/lazygrid/MainActivity.kt
+++ app/src/main/java/com/debduttapanda/lazygrid/MainActivity.kt
@@ -3,16 +3,16 @@ package com.debduttapanda.lazygrid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -21,7 +21,6 @@ import androidx.compose.ui.unit.sp
 import com.debduttapanda.lazygrid.ui.theme.LazyGridTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -29,28 +28,83 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = Color(0xff1a1a1a)
                 ) {
-                    val data = listOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5")
-
-                    LazyHorizontalGrid(
-                        rows = androidx.compose.foundation.lazy.grid.GridCells.Fixed(3),
-                        contentPadding = PaddingValues(8.dp)
-                    ) {
-                        items(data) { item ->
-                            Card(
-                                modifier = Modifier.padding(4.dp),
-                                backgroundColor = Color.LightGray
-                            ) {
-                                Text(
-                                    text = item,
-                                    fontSize = 24.sp,
-                                    textAlign = TextAlign.Center,
-                                    modifier = Modifier.padding(24.dp)
-                                )
-                            }
+                    Box(){
+                        Column(){
+                            LazyHorizontalGrid()
+                            LazyVerticalGrid()
                         }
                     }
+
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun ColumnScope.LazyVerticalGrid() {
+        val data = MutableList<String>(20){
+            it.toString()
+        }.toList()
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            contentPadding = PaddingValues(8.dp),
+            modifier = Modifier.fillMaxSize().weight(1f)
+        ) {
+
+            items(data) { item ->
+                Card(
+                    modifier = Modifier.padding(4.dp),
+                    backgroundColor = Color(0xff7400e8)
+                ) {
+                    Box(
+                        modifier = Modifier.wrapContentSize(),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Text(
+                            text = item,
+                            fontSize = 24.sp,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.padding(24.dp),
+                            color = Color.White
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun ColumnScope.LazyHorizontalGrid() {
+        val data = MutableList<String>(20){
+            it.toString()
+        }.toList()
+
+        LazyHorizontalGrid(
+            rows = GridCells.Fixed(3),
+            contentPadding = PaddingValues(8.dp),
+            modifier = Modifier.fillMaxSize().weight(1f)
+        ) {
+            items(data) { item ->
+                Card(
+                    modifier = Modifier.padding(4.dp),
+                    backgroundColor = Color(0xff7400e8)
+                ) {
+                    Box(
+                        modifier = Modifier.wrapContentSize(),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Text(
+                            text = item,
+                            fontSize = 24.sp,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.padding(24.dp),
+                            color = Color.White
+                        )
+                    }
+
                 }
             }
         }
